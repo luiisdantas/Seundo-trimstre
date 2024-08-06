874f00a627581b9a3cc7f84f80d3b108432bbb6e
luiisdantas <126833690+luiisdantas@users.noreply.github.com>
2024-08-06 19:02:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,67 +6,67 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "Qual foi o impacto imediato da bomba atômica no mundo?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Desencadeou o fim da Segunda Guerra Mundial de forma abrupta.",
+                afirmacao: "A bomba atômica acelerou o fim da Segunda Guerra Mundial, marcando uma virada na história global."
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: "Provocou uma corrida armamentista entre as potências mundiais.",
+                afirmacao: "A criação da bomba atômica desencadeou uma corrida armamentista entre as nações, aumentando as tensões globais."
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "Como a existência da bomba atômica influenciou as relações internacionais durante a Guerra Fria?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: "Intensificou a competição entre os Estados Unidos e a União Soviética.",
+                afirmacao: "A ameaça da bomba atômica intensificou a Guerra Fria, moldando estratégias políticas e militares das superpotências."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Promoveu a cooperação internacional para o controle de armas nucleares.",
+                afirmacao: "A existência da bomba atômica motivou esforços internacionais para o controle e redução de armas nucleares."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "Como a proliferação das armas nucleares impactou a segurança global?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Aumentou as preocupações com a segurança e a estabilidade mundial.",
+                afirmacao: "A proliferação de armas nucleares aumentou as preocupações com a segurança global, levando a esforços para evitar a sua disseminação."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Contribuiu para a manutenção da paz e da estabilidade entre as nações.",
+                afirmacao: "Alguns argumentam que a posse de armas nucleares por diversas nações ajudou a manter a estabilidade global, através da dissuasão."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "Como a bomba atômica influenciou o desenvolvimento de tecnologias e políticas de energia nuclear?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Impulsionou o desenvolvimento de programas nucleares para fins energéticos e militares.",
+                afirmacao: "A bomba atômica impulsionou o desenvolvimento de tecnologias nucleares tanto para a geração de energia quanto para fins militares."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Reduziu o investimento em energia nuclear devido aos riscos associados.",
+                afirmacao: "A ameaça representada pela bomba atômica levou a um debate sobre os riscos associados à energia nuclear, influenciando políticas globais."
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "Qual é a perspectiva atual sobre o uso de armas nucleares no cenário internacional?",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Existe uma pressão crescente para o desarmamento nuclear global.",
+                afirmacao: "Atualmente, há uma pressão internacional crescente para o desarmamento nuclear global, devido aos riscos de proliferação e catastróficos."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Permanece um elemento essencial na estratégia de defesa de muitas nações.",
+                afirmacao: "Algumas nações continuam a ver as armas nucleares como um elemento crucial de sua estratégia de defesa, apesar das críticas globais."
             }
         ]
     },
@@ -105,7 +105,7 @@ function respostaSelecionada(opcaoSelecionada) {
 }
 
 function mostraResultado() {
-    caixaPerguntas.textContent = "Em 2049...";
+    caixaPerguntas.textContent = "Impacto da bomba atômica no mundo atual:";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
 }
